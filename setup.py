e25f6f3eaf747bd6b4d3c8098ced022076195c9f
Wren Wightman <wren.wightman@duke.edu>
2023-08-21 11:49:37 -0400

    made setupe configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import Extension, setup
+
+# run setup tools
+setup(
+    name='pyusel-types',
+    description="class types for pyusel package",
+    author_email="wew12@duke.edu",
+    packages=['pyusel_types'],
+    package_dir={'pyusel_types':'pyusel_types'},
+    license="MIT",
+    version="0.0.0"
+)
