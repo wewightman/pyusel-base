232f4910bb6d1b1cc870595d32513866453b365e
Wren Wightman <wren.wightman@duke.edu>
2023-08-21 16:07:50 -0400

    Make datatype object

--- pyusel_types/data.py
+++ pyusel_types/data.py
@@ -15,22 +15,6 @@ class RawData(ABC):
     
     @classmethod
     @abstractmethod
-    def getdata(self, *args, **kwargs):
-        raise NotImplementedError
-    
-class RawDataSet(ABC):
-    @classmethod
-    @abstractmethod
-    def __init__(self):
-        raise NotImplementedError
-    
-    @classmethod
-    @abstractmethod
-    def transform(self, *args, **kwargs):
-        raise NotImplementedError
-    
-    @classmethod
-    @abstractmethod
     def __getitem__(self, *args, **kwargs):
         raise NotImplementedError
     
